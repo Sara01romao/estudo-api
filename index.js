2034dccc448433342f267f3715a54d275cbb524a
Sara Romão <sara-romao@live.com>
2022-04-23 23:08:32 -0300

    serve express, post e get

---
+++ index.js
@@ -0,0 +1,18 @@
+
+//Criação do serve
+const express = require('express'); //add o modulo no arquivo
+
+const app = express(); //express constante inicializado
+
+app.listen('3000');//fica ouvindo e usa a porta definida. Executa no teminal com node index.js ou node .
+//
+
+
+//função para rotas. Primeiro argumento ROTA depois metodo (requis e respo)
+app.route('/').get((req, res)=>res.send("Hello")); 
+
+
+//middleware - ponte entre o codigo e o insomnia
+app.use(express.json())//transforma em json
+
+app.route('/post').post((req, res)=>res.send(req.body))
