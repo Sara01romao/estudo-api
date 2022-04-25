05a33652a7536aa5686270271f7c96d32734b256
Sara Romão <sara-romao@live.com>
2022-04-24 22:42:56 -0300

    put

--- index.js
+++ index.js
@@ -9,10 +9,21 @@ app.listen('3000');//fica ouvindo e usa a porta definida. Executa no teminal com
 
 
 //função para rotas. Primeiro argumento ROTA depois metodo (requis e respo)
-app.route('/').get((req, res)=>res.send("Hello")); 
-
+/* app.route('/').get((req, res)=>res.send("Hello")); 
+ */
 
 //middleware - ponte entre o codigo e o insomnia
 app.use(express.json())//transforma em json
 
 app.route('/post').post((req, res)=>res.send(req.body))
+
+
+let author = "sara";
+
+app.route('/').get((req, res) => res.send(author))
+
+//put recebe e edita
+app.route('/').put((req, res)=>{
+    author = req.body.author;
+    res.send(author)
+})
